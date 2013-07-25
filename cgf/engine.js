666065dcfc363e746d8b6df65fa06f4b1cc9493a
dogpantz <dogpantz@gmai.com>
2013-07-25 17:10:55 -0600

    Organized engine.js a bit, added comments, changed functions to objects (static)

--- cgf/engine.js
+++ cgf/engine.js
@@ -1,40 +1,16 @@
-function Time() {'use strict';} //Keeps track of the frame rate and delta time. Also includes useful time functions. 
+/*
+### Canvas Wrapper ###
 
-Time.fps = 0; // Dynamic value containing the frame rate.
-Time.delta = 0; // Dynamic value containing the time that passes each frame. 
+Wraps over Canvas api. Easy way to get a canvas and context.
 
-Time.lastFPS = (new Date()).getTime();
-Time.fpsCounter = 0;
-Time.lastFrame = 0;
-
-Time.get = function() {
-    'use strict';
-    return (new Date()).getTime();
-};
-
-//Updates the fps and delta values.
-Time.update = function() {
-    'use strict';
-    if (Time.get() - Time.lastFPS > 1000) {
-        Time.fps = Time.fpsCounter;
-        Time.fpsCounter = 0; //reset the FPS counter
-        Time.lastFPS += 1000; //add one second
-    }
-    Time.fpsCounter++;
-
-    var time = Time.get();
-    var deltaTime = (time - Time.lastFrame);
-    Time.lastFrame = time;
-    if (!(deltaTime >= time))
-        Time.delta = deltaTime;
-};
+*/
 
 function Canvas(canvasID, _useWebGL) {
     'use strict';
-    var id = canvasID,
-    useWebGL = _useWebGL,
-    element = $("#" + id).get(0),
-    context; 
+    var id = canvasID;
+    var useWebGL = _useWebGL;
+    var element = $("#" + id).get(0);
+    var context; 
 
     if (useWebGL) {
     	context = element.getContext("webgl") || element.getContext("experimental-webgl");
@@ -91,19 +67,28 @@ function Canvas(canvasID, _useWebGL) {
     }
 }
 
-function Engine() {'use strict';} //Handles the loop since Javascript doesn't offer threading. 
+/*
+### Engine ###
 
-Engine.canvasList = new Array();
+Object with static methods.
+Runs the game loop, updates all canvases.
+
+*/
+
+//Handles the loop since Javascript doesn't offer threading. 
+Engine = {};
+
+Engine.canvasList = [];
 
 //Initializes everything needed before you start the loop.
 Engine.init = function(frameRate) {
 	//Default values
 	Engine.frameRate = typeof frameRate !== "undefined" ? frameRate : 60;
-}
+};
 
 Engine.add = function(canvas) {
 	Engine.canvasList.push(canvas);
-}
+};
 
 //This is pretty much the loop. 
 Engine.run = function() {
@@ -115,7 +100,47 @@ Engine.run = function() {
 	}
 
 	setTimeout(Engine.run, 1000/Engine.frameRate); //Executes this function again after the time has passed which causes a synced loop to occur.
-}
+};
 
 //These methods will be overridden.
-Engine.update = function() {}
+Engine.update = function() {};
+
+/*
+### Time ###
+
+Useful static methods for time.
+
+*/
+
+Time = {}; //Keeps track of the frame rate and delta time. Also includes useful time functions. 
+
+Time.fps = 0; // Dynamic value containing the frame rate.
+Time.delta = 0; // Dynamic value containing the time that passes each frame. 
+
+Time.lastFPS = (new Date()).getTime();
+Time.fpsCounter = 0;
+Time.lastFrame = 0;
+
+Time.get = function() {
+    'use strict';
+    return (new Date()).getTime();
+};
+
+//Updates the fps and delta values.
+Time.update = function() {
+    'use strict';
+    if (Time.get() - Time.lastFPS > 1000) {
+        Time.fps = Time.fpsCounter;
+        Time.fpsCounter = 0; //reset the FPS counter
+        Time.lastFPS += 1000; //add one second
+    }
+    Time.fpsCounter++;
+
+    var time = Time.get();
+    var deltaTime = (time - Time.lastFrame);
+    Time.lastFrame = time;
+    if (!(deltaTime >= time)) {
+        Time.delta = deltaTime;
+    }
+};
+
