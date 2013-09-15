7d2e893bfd70de5f50e6d5cb13d5ddf44fd24520
dogpantz <dogpantz@gmail.com>
2013-09-15 13:35:23 -0600

    in process of fixing loading bug.

--- cgf/loader.js
+++ cgf/loader.js
@@ -58,18 +58,24 @@ function Loader() {
                 }
             }
 
-            for (var i in files) {     
+            for (var i in files) {
+                console.log("LOADING " + files[i].path);
+                console.log(files[i]);
                 $.ajax({
+                    //TODO: Add converter here to auto convert json, xml
                     url : files[i].path,
                     dataType: "text",
                 }).done(function(data) {
                     // Refer to length of loadedFiles array for current file
                     var loadedCount = loadedFiles.length;
+                    console.log("LOADING FINISHED " + files[loadedCount].id + " " + files[loadedCount].path);
+                    console.log(data.substring(10));
                     loadedFiles.push({
                         id: files[loadedCount].id,
                         path: files[loadedCount].path,
                         data: data
                     });
+                    console.log(loadedFiles);
                     files[loadedCount].callback(data);
                     loadComplete(callback);
                 });
